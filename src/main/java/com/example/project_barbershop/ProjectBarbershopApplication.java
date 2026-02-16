5e66d81a144159248f145e0886a412e6ae461382
User <ovadiayonatan0@gmail.com>
2026-02-16 20:52:49 +0200

    Initial commit

---
+++ src/main/java/com/example/project_barbershop/ProjectBarbershopApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_barbershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBarbershopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBarbershopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/project_barbershop/ProjectBarbershopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_barbershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBarbershopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
